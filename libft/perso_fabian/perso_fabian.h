413828ad1c0610f598e3cea48f6a3e2bf30df7b4
Marco Belarbi <mbelarbi@student.42lausanne.ch>
2022-04-12 17:06:30 +0200

    La libft est terminée et fonctionnelle. V1.1

---
+++ libft/perso_fabian/perso_fabian.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facolomb <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/27 09:17:14 by facolomb          #+#    #+#             */
+/*   Updated: 2022/04/12 15:10:11 by Marco Belarbi    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PERSO_FABIAN_H
+# define PERSO_FABIAN_H
+# include <unistd.h>
+# include <stdlib.h>
+# include "../libft42/libft42.h"
+# include "../printf/ft_printf.h"
+# include "../perso_marco/perso_marco.h"
+
+long	ft_atoi_long(const char *str);
+int		ft_uitoa(unsigned int n);
+int		ft_itohex(unsigned int num, char c);
+int		ft_isalldigit(char *str);
+#endif
