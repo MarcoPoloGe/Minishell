c5026907c9f963d6a12251fb7b149a429e4a4539
facolomb <facolomb@student.42lausanne.ch>
2022-06-08 15:25:24 +0200

    modif utilisation de global et get_redir_path ok

--- src/new_parser.c
+++ src/new_parser.c
@@ -11,50 +11,19 @@
 /* ************************************************************************** */
 #include "minishell.h"
 
-int	ft_is_token_match(char *token)
-{
-	int		is_match;
-	char	**to_match;
-
-	is_match = 0;
-	to_match = ft_calloc(6, sizeof (char *));
-	to_match[0] = ft_strdup("PIPE");
-	to_match[1] = ft_strdup("GREAT");
-	to_match[2] = ft_strdup("GREATGREAT");
-	to_match[3] = ft_strdup("LESS");
-	to_match[4] = ft_strdup("LESSLESS");
-	while (*to_match)
-	{
-		if (ft_str_same(token, *to_match))
-		{
-			is_match = 1;
-			break ;
-		}
-		to_match++;
-	}
-	ft_free_tab(to_match);
-	return (is_match);
-}
-
-void ft_manage_fd(t_cmd_table *table, int i)
-{
-
-}
-
 void ft_add_cmd_params(char **tokens, t_cmd_table *table, int i, int *j)
 {
 	ft_tabadd(&table->cmd_array[i].args, table->cmd_array[i].cmd);
-	if (ft_is_token_match(tokens[*j - 1]))
+	if (ft_is_redir(tokens[*j - 1]))
 		if (ft_str_same(tokens[*j - 1], "LESSLESS"))
 			ft_extract_fd(tokens[*j]);
 		else
 			ft_tabadd(&table->cmd_array[i].args, tokens[(*j)++]);
 	else
 	{
-		while (tokens[*j] && !ft_is_token_match(*tokens))
+		while (tokens[*j] && !ft_is_redir(*tokens))
 			ft_tabadd(&table->cmd_array[i].args, tokens[(*j)++]);
 	}
-	ft_manage_fd(table, i);
 }
 
 int	ft_nb_cmd(char **tokens)
@@ -64,14 +33,14 @@ int	ft_nb_cmd(char **tokens)
 	i = 1;
 	while (*tokens)
 	{
-		if (ft_is_token_match(*tokens))
+		if (ft_is_redir(*tokens))
 			i++;
 		tokens++;
 	}
 	return (i);
 }
 
-t_cmd_table	*ft_new_parser(char **tokens, char **env, char *bin_folder, char *redir_folder)
+t_cmd_table	*ft_new_parser(char **tokens, char **env)
 {
 	t_cmd_table	*table;
 	int			i;
@@ -82,17 +51,17 @@ t_cmd_table	*ft_new_parser(char **tokens, char **env, char *bin_folder, char *re
 	table = ft_init_table(ft_nb_cmd(tokens), env);
 	while (i < table->cmd_count)
 	{
-		table->cmd_array[i].cmd = ft_get_redir_path(tokens[j], redir_folder);
+		table->cmd_array[i].cmd = ft_get_redir_path(tokens[j]);
 		if (table->cmd_array[i].cmd != NULL)
 		{
-			if (!(ft_is_token_match(tokens[++j])))
+			if (!(ft_is_redir(tokens[++j])))
 				ft_add_cmd_params(tokens, table, i, &j);
 			else{}
 				//TODO ERROR
 		}
 		else
 		{
-			table->cmd_array[i].cmd = ft_get_cmd_path(env, tokens[j++], bin_folder);
+			table->cmd_array[i].cmd = ft_get_cmd_path(env, tokens[j++]);
 			if (table->cmd_array[i].cmd != NULL)
 				ft_add_cmd_params(tokens, table, i, &j);
 			else {}
---
+++ src/parser_utils/get_redir_path.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_redir_path.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facolomb <facolomb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 12:03:34 by facolomb          #+#    #+#             */
+/*   Updated: 2022/06/08 12:03:34 by facolomb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../minishell.h"
+
+char	*ft_find_redir_path(char ***redirs, char *str)
+{
+	int i;
+	char *result;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (ft_str_same(redirs[i][0], str))
+			return (ft_strjoin(REDIR_FOLDER, redirs[i][1]));
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_get_redir_path(char *str)
+{
+	char	***redirs;
+	char 	*redir_path;
+
+	redirs = ft_read_two_way_tab_file(REDIR_WORDS_FILE, "txt",'=');
+	if (!redirs)
+		return (NULL);
+	redir_path = ft_find_redir_path(redirs, str);
+	return (redir_path);
+}
