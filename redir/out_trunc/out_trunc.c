d18b9683849eeab26e750948ae53c0bdbf3b9d6e
Marco Belarbi <mbelarbi@student.42lausanne.ch>
2022-06-20 18:03:56 +0200

    Ajout des programes de redirections: in_file, in_prompt, out_append et out_trunc.

---
+++ redir/out_trunc/out_trunc.c
@@ -0,0 +1,27 @@
+
+#include "../../src/minishell.h"
+
+void	ft_out_trunc(char *str)
+{
+	int		fd;
+	char	*file;
+
+	fd = open(str, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+	if (fd < 0)
+		ft_putstr_fd("Error out_trunc: file opening or creation.", 2);
+	file = ft_read_fd(0);
+	if(isatty(1))
+		ft_putstr_fd(file, fd);
+	else
+		ft_putstr_fd(file, 1);
+	close(fd);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		ft_printf_fd(2, "Error : not one arg for redir out_append.");
+	else
+		ft_out_trunc(argv[1]);
+	return (0);
+}
