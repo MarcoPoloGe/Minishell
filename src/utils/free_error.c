07465210bbd3dd3b4a4175cf3d3820a556882f05
Marco Belarbi <mbelarbi@student.42lausanne.ch>
2022-06-28 14:59:18 +0200

    added function ft_check_config_files() for main()

--- src/utils/free_error.c
+++ src/utils/free_error.c
@@ -12,6 +12,14 @@
 
 #include "../minishell.h"
 
+void ft_check_config_files(void)
+{
+	struct stat buffer;
+
+	if(stat(REDIR_WORDS_FILE, &buffer) == -1 || stat(META_WORDS_FILE, &buffer) == -1)
+		ft_fatal_error("can't find configuration files", NULL, NULL);
+}
+
 void	ft_free_struct(t_cmd_table **table_adress)
 {
 	t_cmd_table	*table;
